5752d4bfe6b694e7e27749ab72b11c7fa4b21360
Alfredo hernandez <arlequine666.ah@gmail.com>
2024-11-11 14:32:58 -0600

    fix(sidebar):add logout link

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,14 +4,23 @@ import React, { createContext, useState, ReactNode } from 'react';
 interface AuthContextType {
   userInfo: UserInfoType;
   setUserInfo: (info: UserInfoType) => void;
+  logout: () => void;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [userInfo, setUserInfo] = useState<UserInfoType | null>(null);
+
+
+  const logout = () => {
+    setUserInfo(null);
+    localStorage.removeItem('userInfo');
+  }
+
+
   return (
-    <AuthContext.Provider value={{ userInfo: userInfo!, setUserInfo }}>
+    <AuthContext.Provider value={{ userInfo: userInfo!, setUserInfo, logout }}>
       {children}
     </AuthContext.Provider>
   );
